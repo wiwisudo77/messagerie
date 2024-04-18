2f1c9a76be8ca169c0c1427f0b029b10741931f1
wilfried M <weasytech77@gmail.com>
2024-04-18 19:34:30 +0200

    Gestion de la notification d'un utilisateur

--- admin/administration.php
+++ admin/administration.php
@@ -3,5 +3,6 @@
 echo "Developpement de la fonction permettant de créer un utilisateur";
 echo "Developpement de la fonction de modération des utilisateur";
 echo "Developpement de la fonction de réinitialisation d\'un mot de passe";
+echo "Developpement de la fonction permettant de supprimer un utilisateur";
 
 ?>
