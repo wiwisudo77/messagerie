5b5a3b8ff13d9e795abe1592015a6abe4b46c645
wilfried M <weasytech77@gmail.com>
2024-04-18 19:32:08 +0200

    Gestion des mots de passe

--- admin/administration.php
+++ admin/administration.php
@@ -2,5 +2,6 @@
 
 echo "Developpement de la fonction permettant de créer un utilisateur";
 echo "Developpement de la fonction de modération des utilisateur";
+echo "Developpement de la fonction de réinitialisation d\'un mot de passe";
 
 ?>
